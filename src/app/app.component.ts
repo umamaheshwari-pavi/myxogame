26045e8fb20c0b703847fe59c0d82ae319a4f2a6
umamaheshwari-pavi <umapavi200012@gmail.com>
2025-05-15 01:57:01 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+//https://chatgpt.com/c/67ecbceb-0d24-800a-bc7f-207acc46c335
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,56 +9,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  board:string[] =Array(9).fill('');
-  currentPlayer:string ='x';
-  Winner:string |null= null;
+  board:string[] =["","","","","","","","",""]
+  winner :string|null=null;
+  currentPlayer:string='x'
+  
+  makemove(i:any){
 
-  makeMove(index:number){
+    if(this.board[i] || this.winner){
+      alert("Enter in the other box or Reset the game")
+      return;
+      }
+      else{
+        this.board[i]= this.currentPlayer
+      }
 
-    if(this.board[index] || this.Winner){
-      alert('enter in some other box or reset')
-    } 
-    else{
-      this.board[index] = this.currentPlayer; 
-    }
+      if(this.checkWinner()){
+        this.winner=this.currentPlayer;
+      }
+      else if(!this.board.includes("")){
+        this.winner="No one its draw";
+      }
+      else{
+        this.currentPlayer=this.currentPlayer==='x' ?'o':'x'
+      }
 
-    if(this.checkWinner()){
-      this.Winner =this.currentPlayer;
-    
-    }
-    else{
-      this.currentPlayer=this.currentPlayer === 'x' ? 'o' : 'x';
-
-    }
 
   }
 
-  ////////////////// logics////////////////////////////
-
-    checkWinner():boolean{
-
-      const winPattern = [
-        [0,1,2], [3,4,5], [6,7,8],
-        [0,3,6], [1,4,7], [2,5,8], 
-      [0,4,8], [2,4,6], 
-       ]
-
-      return winPattern.some(([a,b,c])=> 
-       
-        this.board[a] &&
-        this.board[a] === this.board[b] && 
-        this.board[a] === this.board[c]
+  checkWinner():boolean{
+    const winPattern = [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8], 
+      [0,4,8], [2,4,6]
+     ]
+     return winPattern.some(([x,y,z])=>{
+    return this.board[x] && this.board[x] === this.board[y] && this.board[x] === this.board[z]
         
-      )
-    }
+     })
+  }
 
-    resetGame(){
-      this.board.fill('')
-      this.currentPlayer='x';
-      this.Winner=null;
-    }
+
+  resetgame(){
+  this.board=    ["","","","","","","","",""];
+  this.winner=null;
+  this.currentPlayer='x';
+  }
 
 
   
-
 }
